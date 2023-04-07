b9f2f5ced4f6d6ecd7d4eba0ed8b74ee01e8e7c2
azkelif <elifazak83@gmail.com>
2023-04-07 17:59:45 +0300

    temel hatlar oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app_jam/square.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,14 +31,75 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          MySquare(),
-          MySquare(),
-          MySquare(),
-          MySquare(),
-        ],
+    drawer:  Drawer(child: Column(mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          height: 100,
+           child: ElevatedButton(
+             
+             style: ElevatedButton.styleFrom(backgroundColor: Color.fromARGB(255, 255, 255, 255)),
+             onPressed: () {},
+             child: Center(
+               child: Text("Kullanıcıyı Engelle",
+               style:TextStyle(color: Colors.red,fontSize: 20,fontWeight: FontWeight.bold),
+               ),
+             ),
+           ),
+           
+         ),
+      ],
+    )),
+        body: ListView(
+          children: [SizedBox(
+            height: 80,
+            width: 80,
+            child: Text("ETKİNLİK İSMİ",
+            textScaleFactor: 1.5,),
+            //Etkinlik ismi yazılacak
+          ),
+            SizedBox(
+              height: 50,
+              width: 50,
+              child: Text("ETKİNLİK AÇIKLAMASI",
+              textScaleFactor: 1.5,),
+            //etkinlik açıklaması yazılacak
+          ),
+            Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: Container(
+        //ETKİNLİK AYRINTILARI YAZILACAK
+        height: 200,
+        decoration: BoxDecoration(color: Colors.grey,borderRadius: BorderRadius.circular(20)),
+        
       ),
-    );
+    ),
+    Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      child: Container(
+        //KAÇ KİŞİNİN GELECEĞİ YAZACAK
+        height: 80,
+        decoration: BoxDecoration(color: Colors.grey,borderRadius: BorderRadius.circular(20)),
+        
+      ),
+    )
+            
+          ],
+        ),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 96, 203, 238),
+          title: Center(
+              child: Text(
+            "Etkinlik Detay",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25),
+          )),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.message_outlined),
+            ),
+            
+          ],
+        ));
   }
 }
