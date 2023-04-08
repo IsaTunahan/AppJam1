9df9bd9c4c08ffc6719234c8c44f63468e6cb446
IsaTunahan <118114545+IsaTunahan@users.noreply.github.com>
2023-04-08 18:39:23 +0300

    Şifremi unuttum düzeltildi

--- lib/pages/forgot_pw_page.dart
+++ lib/pages/forgot_pw_page.dart
@@ -1,18 +1,106 @@
+import 'package:app_jam/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../widgetlar/_button.dart';
+import '../widgetlar/_textfield.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
-  const ForgotPasswordPage({super.key});
+  ForgotPasswordPage({super.key});
 
   @override
   State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final emailcontroller = TextEditingController();
+  @override
+  void dispose() {
+    emailcontroller.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailcontroller.text.trim());
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+        context: context,
+        builder: (context) {
+          return const AlertDialog(
+            content: Text(
+                'Şifre yenileme bağlantısı gönderildi! Lütfen emailinizi kontrol edin.'),
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Lütfen şifre yenileme bağlantısı için kayıtlı e-posta adresinizi girin.')
-    );
+        appBar: AppBar(
+          title: Text('Şifreni mi unuttun?'),
+          backgroundColor: AppColors.blue,
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(25),
+                child: Text(
+                    'Lütfen şifre yenileme bağlantısı için kayıtlı e-posta adresinizi girin.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.blue,
+                      fontSize: 20,
+                    )),
+              ),
+              AppTextField(
+                controller: emailcontroller,
+                hintText: 'Email',
+                obscureText: false,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 25),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: GestureDetector(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              passwordReset();
+                            },
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: AppColors.red,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(15))),
+                            child: const Padding(
+                              padding: EdgeInsets.symmetric(vertical: 10),
+                              child: Text(
+                                'Giriş Yap',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
