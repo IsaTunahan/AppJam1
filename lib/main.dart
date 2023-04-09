4afc4729c033f7d4aa75570c10c7dc29e45e5f1a
Derya Telli <124207752+DeryaTelli@users.noreply.github.com>
2023-04-09 16:26:04 +0300

    derya klasorune alindi tum dosyalar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app_jam/bottomNavigationBar/HomePage.dart';
+import 'package:app_jam/derya/bottomNavigationBar/HomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
