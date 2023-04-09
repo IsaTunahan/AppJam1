e457a93512cd7d444b19f8d5ada88d0e617b3a05
azkelif <elifazak83@gmail.com>
2023-04-09 15:24:12 +0300

    mesajlaşma uygulaması yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 //import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -36,9 +35,38 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   //late GoogleMapController mapController;
   //final LatLng _center = const LatLng(37.7749, -122.4194);
+  int kisi_sayisi = 0;
+  bool isButtonClikable = true;
+  bool isButtonClikable1 = false;
+
+  void ButtonFunction() {
+    setState(() {
+      isButtonClikable = false;
+    });
+  }
+
+  void ButtonFunction1() {
+    setState(() {
+      isButtonClikable1 = true;
+    });
+  }
+
+  void kisi_increase() {
+    setState(() {
+      kisi_sayisi++;
+    });
+  }
+
+  void kisi_decrease() {
+    setState(() {
+      kisi_sayisi--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(233, 245, 237, 237),
         drawer: Drawer(
             child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -47,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 100,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 255, 255, 255)),
+                    backgroundColor: const Color.fromARGB(255, 255, 255, 255)),
                 onPressed: () {},
                 child: const Center(
                   child: Text(
@@ -67,27 +95,35 @@ class _MyHomePageState extends State<MyHomePage> {
             const SizedBox(
               height: 80,
               width: 80,
-              child: Text(
-                "ETKİNLİK İSMİ",
-                textScaleFactor: 1.5,
+              child: Center(
+                child: Text(
+                  "ETKİNLİK İSMİ",
+                  textScaleFactor: 1.5,
+                ),
               ),
               //Etkinlik ismi yazılacak
             ),
-            
             const SizedBox(
               height: 50,
               width: 50,
-              child: Text(
-                "ETKİNLİK AÇIKLAMASI",
-                textScaleFactor: 1.5,
+              child: Center(
+                child: Text(
+                  "ETKİNLİK AÇIKLAMASI",
+                  textScaleFactor: 1.5,
+                ),
               ),
               //etkinlik açıklaması yazılacak
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
               child: Container(
+                child: Center(
+                    child: Text(
+                  "etkinlik ayrıntıları",
+                  style: TextStyle(fontSize: 30),
+                )),
                 //ETKİNLİK AYRINTILARI YAZILACAK
-                height: 100,
+                height: 150,
                 decoration: BoxDecoration(
                     color: Colors.grey,
                     borderRadius: BorderRadius.circular(20)),
@@ -96,6 +132,11 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
               child: Container(
+                child: Center(
+                  child:
+                      Text("$kisi_sayisi kişi katılıyor!", textScaleFactor: 2),
+                ),
+
                 //KAÇ KİŞİNİN GELECEĞİ YAZACAK
                 height: 80,
                 decoration: BoxDecoration(
@@ -103,7 +144,6 @@ class _MyHomePageState extends State<MyHomePage> {
                     borderRadius: BorderRadius.circular(20)),
               ),
             ),
-            Container(),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
               child: Container(
@@ -116,17 +156,48 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Container(
               padding: const EdgeInsets.only(top: 5.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text("Etkinliğe Katıl"),
-                style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(60, 50),
-                    textStyle: const TextStyle(
-                        fontSize: 28, fontStyle: FontStyle.normal),
-                    primary: Colors.lightGreen,
-                    onPrimary: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20))),
+              child: Opacity(
+                opacity: isButtonClikable ? 1.0 : 0.5,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (isButtonClikable) {
+                      kisi_increase();
+                      ButtonFunction();
+                    }
+                  },
+                  // ignore: sort_child_properties_last
+                  child: Text("KATIL"),
+
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(60, 50),
+                      textStyle: const TextStyle(
+                          fontSize: 28, fontStyle: FontStyle.normal),
+                      primary: Colors.lightGreen,
+                      onPrimary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+              child: Opacity(
+                opacity: isButtonClikable ? 0.0 : 1.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    kisi_decrease();
+                    isButtonClikable = true;
+                  },
+                  child: Text("VAZGEÇ"),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(60, 50),
+                      textStyle: const TextStyle(
+                          fontSize: 28, fontStyle: FontStyle.normal),
+                      primary: Colors.lightGreen,
+                      onPrimary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))),
+                ),
               ),
             )
           ],
@@ -141,10 +212,52 @@ class _MyHomePageState extends State<MyHomePage> {
           )),
           actions: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) {
+                  return Mesaj_sayfasi();
+                }));
+              },
               icon: Icon(Icons.message_outlined),
             ),
           ],
         ));
+  }
+}
+
+class Mesaj_sayfasi extends StatefulWidget {
+  const Mesaj_sayfasi({super.key});
+
+  @override
+  State<Mesaj_sayfasi> createState() => _Mesaj_sayfasiState();
+}
+
+class _Mesaj_sayfasiState extends State<Mesaj_sayfasi> {
+  TextEditingController t1 = TextEditingController();
+  List mesajlar = [];
+  gonder() {
+    setState(() {
+      mesajlar.add(t1.text);
+      t1.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(child: ListView.builder(itemCount: mesajlar.length,
+          itemBuilder: (context, indexno) => Text(mesajlar[indexno]),)
+          ),
+          TextField(controller: t1,),
+          ElevatedButton(onPressed: gonder, child: Text("gönder"))
+        ],
+        
+        ),
+        ),
+       
+       );
   }
 }
